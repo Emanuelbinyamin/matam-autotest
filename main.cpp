400abbd41b61b4ff579b9eb2dc0dbcccc75af03c
Emanuel Binyamin <eb1280345@gmail.com>
2026-05-17 11:55:20 +0300

    feat: implement Valgrind Whisperer for clean memory leak parsing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 int main() {
+    int* bad_pointer = new int[50]; // Allocate memory
+    bad_pointer[0] = 42;            // USE the variable so the compiler is happy!
+    
     int num;
     if (std::cin >> num) {
         std::cout << num * 2 << "\n";
     }
-    return 0;
+    
+    // We still forgot to do: delete[] bad_pointer;
+    return 0; 
 }
